3588e95ae7b9c5b2b58b7ef5ae9ef2f6e5a6c5b0
Will Woods <wwoods@redhat.com>
2012-09-29 15:29:03 -0400

    make 'build_scripts' drop .py prefix from scripts

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
 from distutils.core import setup, Command
+from distutils.util import convert_path
+from distutils.command.build_scripts import build_scripts
+from distutils import log
 
 import os
-from os.path import join
+from os.path import join, basename
 from subprocess import check_call
 
 class Gettext(Command):
@@ -49,6 +52,17 @@ class Msgfmt(Command):
                 mo = po[:-3]+'.mo'
                 check_call(['msgfmt', '-vv', po, '-o', mo])
 
+class BuildScripts(build_scripts):
+    def run(self):
+        build_scripts.run(self)
+        for script in self.scripts:
+            script = convert_path(script)
+            outfile = join(self.build_dir, basename(script))
+            if os.path.exists(outfile) and outfile.endswith(".py"):
+                newfile = outfile[:-3] # drop .py
+                log.info("renaming %s -> %s", outfile, basename(newfile))
+                os.rename(outfile, newfile)
+
 setup(name="fedup",
       version="0.1",
       description="Fedora Upgrade",
@@ -59,9 +73,10 @@ setup(name="fedup",
       download_url="https://github.com/wgwoods/fedup/downloads",
       license="GPLv2+",
       packages=["fedup"],
-      scripts=["fedup-cli"],
+      scripts=["fedup-cli.py"],
       cmdclass={
         'gettext': Gettext,
         'msgfmt': Msgfmt,
+        'build_scripts': BuildScripts,
         }
       )
