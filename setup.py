797455c010a7be21d897ce6fcb7fcbc27feb6bff
Will Woods <wwoods@redhat.com>
2012-09-28 23:41:58 -0400

    add custom setup.py commands for handing translations
    'gettext' creates the .pot file and 'msgfmt' will create .mo from .po.

--- setup.py
+++ setup.py
@@ -1,6 +1,52 @@
 #!/usr/bin/python
 
-from distutils.core import setup
+from distutils.core import setup, Command
+
+import os
+from os.path import join
+from subprocess import check_call
+
+class Gettext(Command):
+    description = "Use po/POTFILES.in to generate po/<name>.pot"
+    user_options = []
+    def initialize_options(self):
+        self.encoding = 'UTF-8'
+        self.po_dir = 'po'
+        self.add_comments = True
+
+    def finalize_options(self):
+        pass
+
+    def _xgettext(self, opts):
+        name = self.distribution.get_name()
+        version = self.distribution.get_version()
+        email = self.distribution.get_author_email()
+        cmd = ['xgettext', '-j', '--default-domain', name, '--package-name', name,
+               '--package-version', version, '--msgid-bugs-address', email,
+               '--output', join(self.po_dir, name + '.pot')]
+        if self.add_comments:
+            cmd.append('--add-comments')
+        check_call(cmd + opts)
+
+    def run(self):
+        self._xgettext(['--language=Python', '-f', 'po/POTFILES.in'])
+
+class Msgfmt(Command):
+    description = "Generate po/*.mo from po/*.po"
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        po_dir = 'po'
+        for po in os.listdir(po_dir):
+            po = join(po_dir, po)
+            if po.endswith('.po'):
+                mo = po[:-3]+'.mo'
+                check_call(['msgfmt', '-vv', po, '-o', mo])
 
 setup(name="fedup",
       version="0.1",
@@ -13,4 +59,8 @@ setup(name="fedup",
       license="GPLv2+",
       packages=["fedup"],
       scripts=["fedup-cli"],
+      cmdclass={
+        'gettext': Gettext,
+        'msgfmt': Msgfmt,
+        }
       )
