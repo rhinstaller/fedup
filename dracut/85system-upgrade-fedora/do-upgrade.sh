29d9f1a078484fbb0957e377479ab8e46b5cce86
Will Woods <wwoods@redhat.com>
2012-10-17 17:30:47 -0400

    dracut: updated system-upgrade-fedora module
    Our module does two things:
    
    First, it saves initramfs at $UPGRADEROOT, so we can get back here after
    the system mounts its disks.
    
    Once the system mounts its disks it will:
      1) manipulate $UPGRADEROOT to change default.target to upgrade.target,
      2) recursively bind-mount / to $UPGRADEROOT/sysroot, and
      3) switch-root back into initramfs.
    but that happens outside the scope of this module.
    
    Once we've re-entered initramfs, this module uses the 'upgrade' hook
    to actually run the upgrade tool and upgrade the system. Yay!

---
+++ dracut/85system-upgrade-fedora/do-upgrade.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# actually perform the upgrade
+
+/usr/lib/systemd/fedora-system-upgrade --root=/sysroot --plymouth --debug
--- dracut/85system-upgrade-fedora/module-setup.sh
+++ dracut/85system-upgrade-fedora/module-setup.sh
@@ -12,8 +12,30 @@ depends() {
 }
 
 install() {
+    # stuff we need for initial boot
+    # ------------------------------
+    # SELinux policy
+    dracut_install /etc/selinux/config
+    dracut_install /etc/selinux/*/contexts/files/*
+    dracut_install /etc/selinux/*/policy/*
+    # script to save initramfs at UPGRADEROOT
+    inst_hook pre-pivot 99 "$moddir/keep-initramfs.sh"
+
+    # stuff we use in upgrade hook(s)
+    # -------------------------------
+    # upgrader binary
     inst_binary $UPGRADEBIN
+
+    # RPM hash/sig checks (via NSS) don't work without these
     inst_libdir_file "libfreebl*" "libsqlite*" "libsoftokn*"
+
+    # RPM can't find the rpmdb without rpmconfig
     rpmconfig=$(find /etc/rpm /usr/lib/rpm -name "rpmrc" -o -name "macros*")
     dracut_install $rpmconfig
+
+    # script to actually run the upgrader binary
+    inst_hook upgrade 50 "$moddir/do-upgrade.sh"
+
+    # workaround: systemd won't reboot without swapoff
+    dracut_install swapoff
 }
