06a57424b15297db97dd211c3039cac6adf35f18
Will Woods <wwoods@redhat.com>
2012-10-17 17:45:33 -0400

    systemd/compat: add upgrade-{prep,switch-root}
    These services actually do the fancy switch-root stuff that is required
    by the system-upgrade-fedora dracut module.
    
    Hopefully there will be native support for these things in systemd in
    the near future.

---
+++ systemd/compat/upgrade-prep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# upgrade-prep.sh - set up upgrade dir for upgrading
+# TODO: this should be done by something like 'systemctl pivot-root'
+
+die() { echo "$@"; exit 1; }
+
+. /run/initramfs/upgrade.conf || die "can't find /run/initramfs/upgrade.conf"
+[ -n "$UPGRADEROOT" ] || die "UPGRADEROOT is not set"
+[ -d "$UPGRADEROOT" ] || die "$UPGRADEROOT does not exist"
+
+echo "binding / into $UPGRADEROOT"
+# bind-mount / into the upgrade chroot so we can upgrade it
+mount --rbind / $UPGRADEROOT/sysroot || die "couldn't bind / into upgrade dir"
+
+echo "switching upgraderoot default target to upgrade.target"
+# switch the upgrade chroot target to upgrade.target
+unitdir=$UPGRADEROOT/lib/systemd/system
+ln -sf upgrade.target $unitdir/default.target
+
+echo "upgrade prep complete"
