b96c615ff94b1936053ce1f469b847f615f0375b
Will Woods <wwoods@redhat.com>
2012-10-18 09:50:59 -0400

    do-upgrade.sh: add boot args for upgrade options
    Added support for the following arguments:
    
      rd.upgrade.test: run the upgrade as a test transaction (--test)
      rd.upgrade.verbose: write messages on the splash (--verbose)
      rd.upgrade.debug: output lots of debugging info (--debug)
      plymouth.enable=0: disable plymouth output (no --plymouth)

--- dracut/85system-upgrade-fedora/do-upgrade.sh
+++ dracut/85system-upgrade-fedora/do-upgrade.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 # actually perform the upgrade
 
-/usr/lib/systemd/fedora-system-upgrade --root=/sysroot --plymouth --debug
+upgradepath=/usr/lib/systemd
+
+do_upgrade() {
+    local args=""
+    getargbool 0 rd.upgrade.test && args="$args --testing"
+    getargbool 0 rd.upgrade.verbose && args="$args --verbose"
+    getargbool 0 rd.upgrade.debug && args="$args --debug"
+
+    # enable plymouth output unless specifically disabled
+    getargbool 1 plymouth.enable && args="$args --plymouth"
+
+    # and off we go...
+    $upgradepath/fedora-system-upgrade --root=/sysroot $args
+}
+
+do_upgrade
