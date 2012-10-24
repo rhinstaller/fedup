f68f4b6d49f6ef0ce722c95c49f44ffaaf4efe8b
Will Woods <wwoods@redhat.com>
2012-10-24 00:43:52 -0400

    make a backup before saving the upgrade log/journal

--- dracut/85system-upgrade-fedora/save-journal.sh
+++ dracut/85system-upgrade-fedora/save-journal.sh
@@ -2,8 +2,15 @@
 
 logpath=/sysroot/var/log/upgrade
 
+simple_backup() {
+    [ -e "$1.old" ] && rm -rf "$1.old"
+    [ -e "$1" ] && mv "$1" "$1.old"
+}
+
 # save the journal
+simple_backup $logpath.journal
 cp -a /run/log/journal $logpath.journal
 
 # write out the plain logfile for people who don't like the journal
+simple_backup $logpath.log
 journalctl -a -m > $logpath.log
