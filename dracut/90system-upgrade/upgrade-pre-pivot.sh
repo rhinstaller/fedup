e0665c601e4a883d5710c07b6e9aa71dde1c636a
Will Woods <wwoods@redhat.com>
2012-10-24 14:16:39 -0400

    Don't create upgrade.conf unless UPGRADEROOT and UPGRADELINK exist
    UPGRADEROOT and UPGRADELINK are the sign that an upgrade should be
    performed - only create upgrade.conf (and twiddle plymouth) if those
    exist in the system about to be booted.
    
    This allows us to include system-upgrade in initramfs without messing
    with normal bootup.

--- dracut/90system-upgrade/upgrade-pre-pivot.sh
+++ dracut/90system-upgrade/upgrade-pre-pivot.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo "UPGRADEROOT=$UPGRADEROOT" > /run/initramfs/upgrade.conf
-echo "UPGRADELINK=$UPGRADELINK" >> /run/initramfs/upgrade.conf
-
-plymouth change-mode --updates && plymouth system-update --progress=0
+if [ -d "${NEWROOT}${UPGRADEROOT}" ] && [ -L "${NEWROOT}${UPGRADELINK}" ]; then
+    echo "UPGRADEROOT=$UPGRADEROOT" > /run/initramfs/upgrade.conf
+    echo "UPGRADELINK=$UPGRADELINK" >> /run/initramfs/upgrade.conf
+    plymouth change-mode --updates && plymouth system-update --progress=0
+fi
