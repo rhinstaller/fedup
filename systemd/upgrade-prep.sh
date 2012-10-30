f9bfe68b540fd55e56ed9e795598a1bdf9aa21c8
Will Woods <wwoods@redhat.com>
2012-10-30 16:15:04 -0400

    add GPLv2+ boilerplate where applicable
    ...and remove the incorrect boilerplate from upgrade-switch-root.target.

--- systemd/upgrade-prep.sh
+++ systemd/upgrade-prep.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 # upgrade-prep.sh - set up upgrade dir for upgrading
-# TODO: this should be done by something like 'systemctl pivot-root'
+#
+# Copyright (C) 2012 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Will Woods <wwoods@redhat.com>
 
 die() { echo "$@"; exit 1; }
 
