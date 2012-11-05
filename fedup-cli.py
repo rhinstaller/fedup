79e9f88816c49d6cc2d327da5a2e1868b1e0c245
Will Woods <wwoods@redhat.com>
2012-11-05 13:02:47 -0500

    Fix traceback without --instrepo
    Thanks to 'garretraziel' for the catch.

--- fedup-cli.py
+++ fedup-cli.py
@@ -176,7 +176,7 @@ def parse_args():
     if not (args.network or args.device or args.iso):
         p.error(_('SOURCE is required (--network, --device, --iso)'))
 
-    if '://' in args.instrepo:
+    if args.instrepo and '://' in args.instrepo:
         args.repos.append(('add', 'instrepo=%s' % args.instrepo))
         args.instrepo = 'instrepo'
 
