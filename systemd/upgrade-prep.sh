c039cc5d43ee8c265a6e0bea663c0601529cd5dc
Will Woods <wwoods@redhat.com>
2012-12-04 18:03:57 -0500

    remove boot option during upgrade (#873065)
    This will remove our boot option just before we start the upgrade.
    
    This further ensures that we'll end up back in the normal system if
    something goes wrong while trying to start the upgrade.
    
    Plus, removing it before the upgrade means there's no possibility of our
    boot arguments ending up in the new system's bootloader config (that's
    bug #876366).

--- systemd/upgrade-prep.sh
+++ systemd/upgrade-prep.sh
@@ -24,6 +24,9 @@ die() { echo "$@"; exit 1; }
 [ -n "$UPGRADEROOT" ] || die "UPGRADEROOT is not set"
 [ -d "$UPGRADEROOT" ] || die "$UPGRADEROOT does not exist"
 
+# remove our boot entry
+new-kernel-pkg --remove fedup
+
 # make target dir for systemd's pivot_root
 mkdir -p $UPGRADEROOT/mnt
 
