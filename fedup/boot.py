5a5773ce00652725f8085d484f9980b5862c2026
Will Woods <wwoods@redhat.com>
2012-12-04 17:35:24 -0500

    add fedup.boot
    It turns out there's some magic in new-kernel-pkg that grubby doesn't
    have; without that stuff grubby sometimes doesn't update the right
    bootloader config.
    
    So here's a module that modifies the bootloader config using
    new-kernel-pkg.

---
+++ fedup/boot.py
@@ -0,0 +1,49 @@
+# fedup.boot - bootloader config modification code for the Fedora Upgrader
+#
+# Copyright (C) 2012 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Will Woods <wwoods@redhat.com>
+
+from subprocess import check_output, PIPE
+
+kernelprefix = "/boot/vmlinuz-"
+
+def kernelver(kernel):
+    if kernel.startswith(kernelprefix):
+        return kernel.split(kernelprefix,1)[1]
+    else:
+        raise ValueError("kernel name must start with '%s'" % kernelprefix)
+
+def add_entry(kernel, initrd, banner=None, kargs=[], makedefault=False):
+    cmd = ["new-kernel-pkg", "--initrdfile", initrd]
+    if banner:
+        cmd += ["--banner", banner]
+    if kargs:
+        cmd += ["--kernel-args", " ".join(kargs)]
+    if makedefault:
+        cmd += ["--makedefault"]
+    cmd += ["--install", kernelver(kernel)]
+    return check_output(cmd, stderr=PIPE)
+
+def remove_entry(kernel):
+    cmd = ["new-kernel-pkg", "--remove", kernelver(kernel)]
+    return check_output(cmd, stderr=PIPE)
+
+def get_default():
+    return check_output(["grubby", "--default-kernel"]).strip()
+
+def set_default(kernel):
+    return check_output(["grubby" , "--set-default", kernel])
