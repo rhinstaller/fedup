7c4febe1bfe6769cd400742d91a28efb08a30b02
Will Woods <wwoods@redhat.com>
2015-04-07 17:30:32 -0400

    add sysinfo.has_product_installed()
    This tells us if we have a product already installed. Handy!

--- fedup2/sysinfo.py
+++ fedup2/sysinfo.py
@@ -1,3 +1,14 @@
 # fedup.sysinfo - get information about the host system
 
 from dnf.rpm import detect_releasever
+
+import rpm
+
+def has_product_installed():
+    """
+    Return a list of rpm.hdr objects that provide system-release-product.
+    In a boolean context, this tells us whether we have a product installed
+    or not.
+    """
+    ts = rpm.ts()
+    return list(ts.dbMatch('provides','system-release-product'))
