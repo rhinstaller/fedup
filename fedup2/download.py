0dbfe7e486e855bfae2c68a6f764d0f6c895d4c0
Will Woods <wwoods@redhat.com>
2015-04-09 18:47:56 -0400

    rewrite download.py
    download.py now contains a Downloader class that will do the downloading
    stuff. Huzzah!
    
    It's untested and full of FIXMEs and TODOs but it's a start.

--- fedup2/download.py
+++ fedup2/download.py
@@ -1,158 +1,123 @@
-#!/usr/bin/env python
-# prototype downloader component for fedup 2.0
+# download.py - DNF-based downloader, for doin' upgrades
+#
+# Copyright (c) 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Will Woods <wwoods@redhat.com>
 
 import os
 import sys
-import argparse
-
 import dnf
 import dnf.cli
-import dnf.exceptions
 
-default_instrepo = '@https://mirrors.fedoraproject.org/metalink?' \
-                   'repo=fedora-install-$releasever&arch=$basearch'
+class Downloader(object):
+    def __init__(self, args):
+        self.args = args
+        self.base = None
 
-def hrsize(numbytes):
-    # dumb placeholder: just return size in MiB
-    return "%dMiB" % ((numbytes>>20)+1)
+    def setup(self):
+        # create dnf object. XXX: not public API
+        base = dnf.cli.cli.BaseCli()
+        # prepare progress callbacks
+        repobar, base.ds_callback = base.output.setup_progress_callbacks()
+        # set releasever before we do anything else
+        base.conf.substitutions['releasever'] = self.args.version
+        # set cachedir before we start messing with repos
+        if args.cachedir is not None:
+            base.conf.cachedir = args.cachedir
+        # ..add CACHEDIR_SUFFIX (e.g. "x86_64/21")
+        # (note: the two return values are the same if you're root - if you're a
+        #  regular user the first one is the user's personal cachedir)
+        base.conf.cachedir, sys_cachedir = dnf.cli.cli.cachedir_fit(base.conf)
+        # read repo config
+        base.read_all_repos()
+        # also create instrepo and add it to the repo config
+        instrepo = dnf.repo.Repo('instrepo', base.conf.cachedir)
+        instrepo.substitutions.update(base.conf.substitutions)
+        def subst(rawstr):
+            return dnf.conf.parser.substitute(rawstr, base.conf.substitutions)
+        # NOTE: the progressbar will trace back if you don't set name
+        instrepo.name = subst("Install Image Repo - $releasever - $basearch")
+        if args.instrepo.startswith('@'):
+            instrepo.metalink = subst(self.args.instrepo[1:])
+        else:
+            instrepo.baseurl = [subst(self.args.instrepo)]
+        base.repos.add(instrepo)
+        # change pkgdir to our target dir
+        base.repos.all().pkgdir = args.datadir
+        # add progress callback
+        base.repos.all().set_progress_bar(repobar)
+        # TODO: subclass dnf.cli.output.CliKeyImport to handle our key behavior
+        #key_import = FedupCliKeyImport()
+        #base.repos.all().set_key_import(key_import)
+        self.base = base
 
-def parse_args():
-    # TODO implement this for real
-    args = argparse.Namespace()
-    args.pkgdir = "/var/lib/system-upgrade"
-    args.version = "rawhide"
-    args.cachedir = None
-    args.instrepo = default_instrepo
-    args.quicktest = True
-    args.ipython = True
-    return args
+    def read_metadata(self):
+        '''read rpmdb to find installed packages, get metadata for new pkgs.'''
+        # TODO handle repos that fail to configure
+        self.base.fill_sack(load_system_repo=True, load_available_repos=True)
 
-def write_packagelist(pkglist, pkgdir):
-    '''
-    Write packages.list into pkgdir, which contains relative paths to
-    all the packages named in pkglist.
-    These are the packages that will be installed when we run the upgrade tool.
-    '''
-    with open(os.path.join(pkgdir,"packages.list"), "w") as f:
-        for p in pkglist:
-            f.write(os.path.relpath(p.localPkg(), pkgdir) + '\n')
+    def find_upgrade_packages(self):
+        '''
+        Find all available upgrades.
+        returns: list of package objects.
+        TODO: distro-sync mode (allowing downgrades)
+        '''
+        rv = self.base.upgrade_all()
+        ok = self.base.resolve()
+        # XXX:should we do something with ok and rv?
+        return self.base.transaction.install_set
 
-def setup_base(args):
-    # create dnf object
-    base = dnf.cli.cli.BaseCli()
+    @staticmethod
+    def _get_handle(self, repo):
+        h = repo.get_handle()
+        h.destdir = self.args.datadir
+        h.fetchmirrors = True
+        h.yumdlist = []
+        h.perform()
+        return h
 
-    # prepare progress callbacks
-    repobar, base.ds_callback = base.output.setup_progress_callbacks()
+    # FIXME needs progress meter and logging
+    def _fetch_file(repo, relpath, local_name=None):
+        handle = self._get_handle(repo)
+        if local_name is None:
+            local_name = os.path.basename(relpath)
+        fullpath = os.path.join(handle.destdir, local_name)
+        with open(fullpath, "w+") as f:
+            librepo.download_url(relpath, f.fileno(), handle)
+        return fullpath
 
-    # set releasever before we do anything else
-    base.conf.substitutions['releasever'] = args.version
-    # set cachedir before we start messing with repos
-    if args.cachedir is not None:
-        base.conf.cachedir = args.cachedir
-    # ..add CACHEDIR_SUFFIX (note: these are the same directory if you're root)
-    base.conf.cachedir, sys_cachedir = dnf.cli.cli.cachedir_fit(base.conf)
+    def download_images(self):
+        '''Download boot images from the 'instrepo' repo.
+        returns two file paths: (kernel, initrd)'''
+        instrepo = self.repos['instrepo']
+        if self.args.nogpgcheck:
+            treeinfo_file = self._fetch_file(instrepo, ".treeinfo")
+        else:
+            treeinfo_signed = self._fetch_file(instrepo, ".treeinfo.signed")
+            treeinfo_file = treeinfo_signed[:-7] # drop ".signed"
+            # FIXME: verify treeinfo_signed, write plaintext to treeinfo_file
+            raise NotImplementedError
+        ti = Treeinfo(treeinfo_file)
+        arch = instrepo.substitutions['basearch']
+        kernel = self._fetch_file(instrepo, ti.get_image(arch, 'kernel'))
+        initrd = self._fetch_file(instrepo, ti.get_image(arch, 'upgrade'))
+        # FIXME checking needs to be better here
+        assert ti.checkfile(kernel, ti.get_image(arch, 'kernel'))
+        assert ti.checkfile(initrd, ti.get_image(arch, 'upgrade'))
 
-    # read repo config
-    base.read_all_repos()
-
-    # also create instrepo and add it to the repo config
-    instrepo = dnf.repo.Repo('instrepo', base.conf.cachedir)
-    instrepo.substitutions.update(base.conf.substitutions)
-
-    def subst(rawstr):
-        return dnf.conf.parser.substitute(rawstr, base.conf.substitutions)
-
-    # NOTE: the progressbar will trace back if you don't set name
-    instrepo.name = subst("Install Image Repo - $releasever - $basearch")
-    if args.instrepo.startswith('@'):
-        instrepo.metalink = subst(args.instrepo[1:])
-    else:
-        instrepo.baseurl = [subst(args.instrepo)]
-    base.repos.add(instrepo)
-
-    # change pkgdir to our target dir
-    base.repos.all().pkgdir = args.datadir
-
-    # add progress callback
-    base.repos.all().set_progress_bar(repobar)
-
-    # TODO: subclass dnf.cli.output.CliKeyImport to handle our key behavior
-    #key_import = FedupCliKeyImport()
-    #base.repos.all().set_key_import(key_import)
-
-    return base
-
-def read_metadata(base):
-    # read rpmdb to find installed packages, get metadata for new pkgs
-    # TODO handle repos that fail to configure
-    base.fill_sack(load_system_repo=True, load_available_repos=True)
-
-def find_upgrade_packages(base, args):
-    # find all available upgrades
-    # TODO: distro-sync mode (allow downgrades)
-    rv = base.upgrade_all()
-    ok = base.resolve()
-    pkglist = base.transaction.install_set
-
-    # debugging: "quicktest" means just do the first 10 packages
-    if args.quicktest:
-        pkglist = sorted(pkglist)[:10]
-
-    return pkglist
-
-def download_packages(base, pkglist):
-    dlprogress = dnf.cli.progress.MultiFileProgressMeter(fo=sys.stdout)
-    base.download_packages(pkglist, progress=dlprogress)
-
-def get_handle(repo, destdir=None):
-    h = repo.get_handle()
-    h.destdir = destdir or repo.pkgdir
-    if os.access(h.metalink_path, os.R_OK):
-        h.mirrorlist = h.metalink_path
-    elif os.access(h.mirrorlist_path, os.R_OK):
-        h.mirrorlist = h.mirrorlist_path
-    h.fetchmirrors = True
-    h.yumdlist = []
-    h.perform()
-    return h
-
-def download_file(relpath, handle=None, local_name=None):
-    if local_name is None:
-        local_name = os.path.basename(relpath)
-    fullpath = os.path.join(handle.destdir, local_name)
-    with open(fullpath, "w+") as f:
-        librepo.download_url(relpath, f.fileno(), handle)
-    return fullpath
-
-if __name__ == '__main__':
-    # TODO: logging setup
-    try:
-        args = parse_args()
-
-        print("reading repo config")
-        base = setup_base(args)
-
-        print("setting up repos")
-        read_metadata(base)
-
-        print("finding upgrades")
-        pkglist = find_upgrade_packages(base, args)
-        print("found %i upgrades (rv=%i, ok=%s)" % (len(pkglist), rv, ok))
-        dlsize = sum(p.size for p in pkglist)
-        print("download size: %s" % hrsize(dlsize))
-
-        print("downloading upgrade packages")
-        download_packages(base, pkglist)
-        write_packagelist(pkglist, args.pkgdir)
-    # TODO: real exception handling obvy
-    except dnf.exceptions.DownloadError as e:
-        print("downloading packages failed, bummer :<")
-    except dnf.exceptions.DepsolveError as e:
-        print("depsolving failed, another bummer")
-    except KeyboardInterrupt:
-        pass
-
-    if args.ipython:
-        h = get_handle(base.repos['instrepo'])
-        from IPython import embed
-        embed()
+    def download_packages(self, pkglist):
+        dlprogress = dnf.cli.progress.MultiFileProgressMeter(fo=sys.stdout)
+        self.base.download_packages(pkglist, progress=dlprogress)
